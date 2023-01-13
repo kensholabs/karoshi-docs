150389d30b19dec239d8ba107fdac8c4263c7783
kensho-soldier <107375134+kensho-soldier@users.noreply.github.com>
2023-01-13 13:45:48 +0000

    name and links

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>KAROSHI DAO</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://docs.karoshi.studio',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/kensholabs/karoshi-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'KAROSHI Docs',
   },
 }
 
