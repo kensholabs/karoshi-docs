493efa0c7fab5119a34e670e86bc353a315438f6
kensho-soldier <107375134+kensho-soldier@users.noreply.github.com>
2023-01-26 09:40:59 +0000

    fix

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,8 +5,8 @@ const config: DocsThemeConfig = {
   logo: (
     <>
       <svg
-        width="180"
-        height="39"
+        width="190"
+        height="41"
         viewBox="0 0 322 72"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
